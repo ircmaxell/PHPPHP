e14d217e2d0fe7cdb6d7aed848d659d7d3c34f88
nikic <nikita.ppv@googlemail.com>
2012-12-30 14:28:02 +0100

    Add support for -f option
    Now it's possible to use php.php as a test executable for run-tests.php

--- php.php
+++ php.php
@@ -4,10 +4,36 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $php = new PHPPHP\PHP;
 
-if (isset($argv[1]) && !isset($argv[2])) {
-    $file = realpath($argv[1]);
-    $php->executeFile($file);
-} elseif (isset($argv[1]) && $argv[1] == '-r') {
-    $code = $argv[2];
-    $php->execute('<?php ' . $code);
+list($options, $args) = parseCliArgs($argv);
+
+if (isset($options['f'])) {
+    $php->executeFile(realpath($options['f']));
+} elseif (isset($options['r'])) {
+    $php->execute('<?php ' . $options['r']);
+} elseif (isset($args[0])) {
+    $php->executeFile(realpath($args[0]));
+} else {
+    echo "Invalid arguments\n";
+}
+
+function parseCliArgs(array $args) {
+    // first element is script name
+    array_shift($args);
+
+    $options = array();
+    $arguments = array();
+
+    $currentOption = null;
+    foreach ($args as $arg) {
+        if ($currentOption) {
+            $options[$currentOption] = $arg;
+            $currentOption = null;
+        } elseif (strlen($arg) == 2 && $arg[0] == '-') {
+            $currentOption = $arg[1];
+        } else {
+            $arguments[] = $arg;
+        }
+    }
+
+    return array($options, $arguments);
 }
