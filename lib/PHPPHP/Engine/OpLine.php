f36a0b385559c7a58c3b35841c87c28225aafbe8
Anthony Ferrara <ircmaxell@gmail.com>
2013-01-03 08:14:50 -0500

    Clean up copying of attributes at @nikic's request

--- lib/PHPPHP/Engine/OpLine.php
+++ lib/PHPPHP/Engine/OpLine.php
@@ -6,13 +6,13 @@ abstract class OpLine {
     public $op1;
     public $op2;
     public $result;
-    public $attributes;
+    public $attributes = array();
 
     public function __construct(\PHPParser_Node $node, $op1 = null, $op2 = null, $result = null) {
         $this->op1    = $op1;
         $this->op2    = $op2;
         $this->result = $result;
-        $this->attributes   = $node->getAttributes();
+        $this->attributes['startLine']   = $node->getLine();
     }
 
     abstract public function execute(\PHPPHP\Engine\ExecuteData $data);
